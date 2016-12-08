aaf6313296b58b4c20ff56a7a7544f07363dc548
Ilija Studen <ilija.studen@activecollab.com>
2016-12-08 20:20:03 +0100

    Add tests

---
+++ test/src/Base/TestCase.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * This file is part of the Active Collab Controller project.
+ *
+ * (c) A51 doo <info@activecollab.com>. All rights reserved.
+ */
+
+namespace ActiveCollab\Controller\Test\Base;
+
+/**
+ * @package ActiveCollab\Controller\Test\Base
+ */
+abstract class TestCase extends \PHPUnit_Framework_TestCase
+{
+}
