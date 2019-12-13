6bedaf2efce0455397b5af82cc71775054a8f596
Ilija Studen <ilija.studen@activecollab.com>
2019-12-13 21:46:04 +0100

    Add (failing) scan method

---
+++ src/FileSystemRouter/FileSystemRouter.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * This file is part of the Active Collab Controller project.
+ *
+ * (c) A51 doo <info@activecollab.com>. All rights reserved.
+ */
+
+declare(strict_types=1);
+
+namespace ActiveCollab\Controller\FileSystemRouter;
+
+class FileSystemRouter
+{
+    public function scan(string $dir): array
+    {
+        return [];
+    }
+}
